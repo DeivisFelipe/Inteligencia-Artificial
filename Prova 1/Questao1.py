03b661f8eeaf0d45df3b04479051e5f23534ae2b
DeivisFelipe <guerreirodosul01@gmail.com>
2023-06-07 16:19:37 -0300

    feat: Criando busca em largura

--- Prova 1/Questao1.py
+++ Prova 1/Questao1.py
@@ -57,9 +57,9 @@ class Estado:
 
         return True
     
-    def printaEstado(self):
+    def printaEstado(self, numeroestado):
         # Header
-        print('F | L | O | R')
+        print('F | L | O | R | Acessado: ' + str(numeroestado))
         # Estado
         print(self.fazendeiro + ' | ' + self.lobo + ' | ' + self.ovelha + ' | ' + self.repolho)
         # Pula linha
@@ -84,27 +84,6 @@ class Estado:
             return False
 
         return True
-    
-    # verifica se o estado é diferente
-    def __ne__(self, other):
-        if self is None and other is None:
-            return False
-        if self is None and other is not None:
-            return True
-        if self is not None and other is None:
-            return True
-
-        if self.fazendeiro != other.fazendeiro:
-            return True
-        if self.lobo != other.lobo:
-            return True
-        if self.ovelha != other.ovelha:
-            return True
-        if self.repolho != other.repolho:
-            return True
-        
-        return False
-        
 
 # Estado inicial
 estado_inicial = Estado('e', 'e', 'e', 'e')
@@ -121,10 +100,94 @@ estados_nao_visitados = []
 # Verifica se terminou
 terminou = False
 
+# Estado acessado número de forma global
+numeroestado = 0
+
 # acessa o estado 
 def acessa_estado_busca_profundidade(estado):
+    global numeroestado
+    # incrementa o numero de estados acessados de forma global
+    numeroestado += 1
     # printa o estado final
-    estado.printaEstado()
+    estado.printaEstado(numeroestado)
+
+    # verifica se o estado é o estado final
+    if estado == estado_final:
+        # termina o programa
+        terminou = True
+        return estado
+
+    # adiciona o estado na lista de estados visitados
+    estados_visitados.append(estado)
+
+    # Gera todos os estados possíveis a partir do estado atual
+    estados_possiveis = gera_estados_possiveis(estado)
+
+    # Inverte a lista de estados possíveis
+    estados_possiveis.reverse()
+
+    # adiciona os estados possíveis na lista de estados não visitados
+    for estadoNovo in estados_possiveis:
+        estados_nao_visitados.append(estadoNovo)
+
+    # Enquanto tiver tiver estados posiíveis, vai dando pop e acessando os estados
+    while len(estados_nao_visitados) > 0:
+        # Pega o primeiro estado da lista de estados não visitados
+        estadoRetirado = estados_nao_visitados.pop(len(estados_nao_visitados) - 1)
+
+        # Acessa o estado
+        resposta = acessa_estado_busca_profundidade(estadoRetirado)
+
+        # verifica se reposta é um estado ou null
+        if resposta is not None:
+            return resposta
+        
+    return None
+
+# Gera todos os estados possíveis a partir do estado atual
+def gera_estados_possiveis(estado):
+    # Lista de estados possíveis
+    estados_possiveis = []
+
+    # Verifica se o fazendeiro está na margem esquerda
+    estado1 = Estado('d' if estado.fazendeiro == 'e' else 'e', estado.lobo, estado.ovelha, estado.repolho)
+
+    estado2 = Estado('d' if estado.fazendeiro   == 'e' else 'e', 'd' if estado.lobo == 'e' else 'e', estado.ovelha, estado.repolho)
+
+    estado3 = Estado('d' if estado.fazendeiro == 'e' else 'e', estado.lobo, 'd' if estado.ovelha == 'e' else 'e', estado.repolho)
+
+    estado4 = Estado('d' if estado.fazendeiro == 'e' else 'e', estado.lobo, estado.ovelha, 'd' if estado.repolho == 'e' else 'e')
+
+    # verifica se não é um estado visitado e se é um estado valido
+    if not verifica_estado_visitado_ou_nao_visitado(estado1) and estado1.verificaValido():
+        estados_possiveis.append(estado1)
+    if not verifica_estado_visitado_ou_nao_visitado(estado2) and estado2.verificaValido():
+        estados_possiveis.append(estado2)
+    if not verifica_estado_visitado_ou_nao_visitado(estado3) and estado3.verificaValido():
+        estados_possiveis.append(estado3)
+    if not verifica_estado_visitado_ou_nao_visitado(estado4) and estado4.verificaValido():
+        estados_possiveis.append(estado4)
+
+    return estados_possiveis
+
+# Verifica se o estado esta dentro de estados visitados
+def verifica_estado_visitado_ou_nao_visitado(estado):
+    for estadoVisitado in estados_visitados:
+        if estado == estadoVisitado:
+            return True
+    
+    for estadoNaoVisitado in estados_nao_visitados:
+        if estado == estadoNaoVisitado:
+            return True
+    
+    return False
+
+# Busca em Largura
+def acessa_estado_busca_largura(estado):
+    global numeroestado
+    numeroestado += 1
+    # printa o estado final
+    estado.printaEstado(numeroestado)
 
     # verifica se o estado é o estado final
     if estado == estado_final:
@@ -145,10 +208,10 @@ def acessa_estado_busca_profundidade(estado):
     # Enquanto tiver tiver estados posiíveis, vai dando pop e acessando os estados
     while len(estados_nao_visitados) > 0:
         # Pega o primeiro estado da lista de estados não visitados
-        estadoRetirado = estados_nao_visitados.pop(len(estados_nao_visitados) - 1)
+        estadoRetirado = estados_nao_visitados.pop(0)
 
         # Acessa o estado
-        resposta = acessa_estado_busca_profundidade(estadoRetirado)
+        resposta = acessa_estado_busca_largura(estadoRetirado)
 
         # verifica se reposta é um estado ou null
         if resposta is not None:
@@ -156,40 +219,19 @@ def acessa_estado_busca_profundidade(estado):
         
     return None
 
-
-
-
-# Gera todos os estados possíveis a partir do estado atual
-def gera_estados_possiveis(estado):
-    # Lista de estados possíveis
-    estados_possiveis = []
-
-    # Verifica se o fazendeiro está na margem esquerda
-    estado1 = Estado('d' if estado.fazendeiro == 'e' else 'e', estado.lobo, estado.ovelha, estado.repolho)
-
-    estado2 = Estado('d' if estado.fazendeiro == 'e' else 'e', 'd' if estado.lobo == 'e' else 'e', estado.ovelha, estado.repolho)
-
-    estado3 = Estado('d' if estado.fazendeiro == 'e' else 'e', estado.lobo, 'd' if estado.ovelha == 'e' else 'e', estado.repolho)
-
-    estado4 = Estado('d' if estado.fazendeiro == 'e' else 'e', estado.lobo, estado.ovelha, 'd' if estado.repolho == 'e' else 'e')
-
-    # verifica se não é um estado visitado e se é um estado valido
-    if estado1 not in estados_visitados and estado1.verificaValido():
-        estados_possiveis.append(estado1)
-    if estado2 not in estados_visitados and estado2.verificaValido():
-        estados_possiveis.append(estado2)
-    if estado3 not in estados_visitados and estado3.verificaValido():
-        estados_possiveis.append(estado3)
-    if estado4 not in estados_visitados and estado4.verificaValido():
-        estados_possiveis.append(estado4)
-
-    return estados_possiveis
-
 # Inicia o programa
 if __name__ == '__main__':
+    numeroestado = 0
     print("Busca em profundidade")
     # Acessa o estado inicial
-    acessa_estado_busca_profundidade(estado_inicial)
+    #acessa_estado_busca_profundidade(estado_inicial)
+
+    # Limpando as listas
+    estados_visitados = []
+    estados_nao_visitados = []
+
+    # Zerando o estado acessado
+    numeroestado = 0
 
     print("Busca em largura")
     # Acessa o estado inicial
