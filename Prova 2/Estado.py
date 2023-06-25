dd72f2473f5af8cd9802bc65de96cfd53200a14a
DeivisFelipe <guerreirodosul01@gmail.com>
2023-06-25 15:04:02 -0300

    feat: minimax funcionando

--- Prova 2/Estado.py
+++ Prova 2/Estado.py
@@ -19,6 +19,9 @@ class Estado:
     inicioY = None
     fimY = None
 
+    # É empate
+    empate = False
+
     # Vencedor do jogo
     vencedor = 0
 
@@ -43,6 +46,7 @@ class Estado:
         self.fimY = self.inicioY + (self.espessura * (self.size - 2))
 
         # Define os centros das celulas
+        self.centros_celulas = []
         for i in range(0, self.size):
             for j in range(0, self.size):
                 self.centros_celulas.append(
@@ -51,15 +55,18 @@ class Estado:
     # Função que desenha o estado
     def desenha(self):
         # Verifica se o jogo acabou
-        if (self.vencedor != 0):
+        if (self.vencedor != 0 or self.empate):
             no_stroke()
             # Escreve o vencedor
             if (self.vencedor == 1):
                 fill(255, 0, 0)
                 text("Vencedor: X", (self.width / 2, 40))
-            else:
+            elif (self.vencedor == 2):
                 fill(0, 255, 0)
                 text("Vencedor: O", (self.width / 2, 40))
+            else:
+                fill(0)
+                text("Empate", (self.width / 2, 40))
         stroke(0)
         stroke_weight(self.borda)
         for i in range(0, self.size - 1):
@@ -106,25 +113,42 @@ class Estado:
         # If se o tamanho for maior que 3, ganha quem fizer 4 em linha
         # Else, ganha quem fizer 3 em linha
         if (self.size > 3):
-            return self.fim4()
+            result = self.fim4()
         else:
-            return self.fim3()
+            result = self.fim3()
+
+        if not result:
+            self.empate = self.verifica_empate()
+            return self.empate
+        else:
+            return result
+
+    def verifica_empate(self):
+        for i in range(0, self.size):
+            for j in range(0, self.size):
+                if (self.matriz[i][j] == 0):
+                    return False
+        return True
 
     # Verifica o fim do jogo para 3 em linha
     def fim3(self):
         for i in range(0, self.size):
             # Verifica se alguma linha tem 3 em linha
             if (self.matriz[i][0] == self.matriz[i][1] and self.matriz[i][1] == self.matriz[i][2] and self.matriz[i][0] != 0):
+                self.vencedor = self.matriz[i][0]
                 return True
 
             # Verifica se alguma coluna tem 3 em linha
             if (self.matriz[0][i] == self.matriz[1][i] and self.matriz[1][i] == self.matriz[2][i] and self.matriz[0][i] != 0):
+                self.vencedor = self.matriz[0][i]
                 return True
 
         # Verifica se alguma diagonal tem 3 em linha
         if (self.matriz[0][0] == self.matriz[1][1] and self.matriz[1][1] == self.matriz[2][2] and self.matriz[0][0] != 0):
+            self.vencedor = self.matriz[0][0]
             return True
         if (self.matriz[0][2] == self.matriz[1][1] and self.matriz[1][1] == self.matriz[2][0] and self.matriz[0][2] != 0):
+            self.vencedor = self.matriz[0][2]
             return True
 
         # Se não tiver nenhuma linha, coluna ou diagonal com 3 em linha, retorna falso
@@ -136,21 +160,50 @@ class Estado:
             for j in range(0, self.size - 3):
                 # Verifica se alguma linha tem 4 em linha
                 if (self.matriz[i][j] == self.matriz[i][j + 1] and self.matriz[i][j + 1] == self.matriz[i][j + 2] and self.matriz[i][j + 2] == self.matriz[i][j + 3] and self.matriz[i][j] != 0):
+                    self.vencedor = self.matriz[i][j]
                     return True
 
                 # Verifica se alguma coluna tem 4 em linha
                 if (self.matriz[j][i] == self.matriz[j + 1][i] and self.matriz[j + 1][i] == self.matriz[j + 2][i] and self.matriz[j + 2][i] == self.matriz[j + 3][i] and self.matriz[j][i] != 0):
+                    self.vencedor = self.matriz[j][i]
                     return True
 
         # Verifica se alguma diagonal tem 4 em linha
         for i in range(0, self.size - 3):
             for j in range(0, self.size - 3):
                 if (self.matriz[i][j] == self.matriz[i + 1][j + 1] and self.matriz[i + 1][j + 1] == self.matriz[i + 2][j + 2] and self.matriz[i + 2][j + 2] == self.matriz[i + 3][j + 3] and self.matriz[i][j] != 0):
+                    self.vencedor = self.matriz[i][j]
                     return True
         for i in range(0, self.size - 3):
             for j in range(3, self.size):
                 if (self.matriz[i][j] == self.matriz[i + 1][j - 1] and self.matriz[i + 1][j - 1] == self.matriz[i + 2][j - 2] and self.matriz[i + 2][j - 2] == self.matriz[i + 3][j - 3] and self.matriz[i][j] != 0):
+                    self.vencedor = self.matriz[i][j]
                     return True
 
         # Se não tiver nenhuma linha, coluna ou diagonal com 4 em linha, retorna falso
         return False
+
+    # Função que pega todos os estados possiveis a partir do estado atual
+    def estados_possiveis(self):
+        estados = []
+        for i in range(0, self.size):
+            for j in range(0, self.size):
+                if (self.matriz[i][j] == 0):
+                    # Cria uma copia do estado atual
+                    novo_estado = Estado(self.size, np.copy(self.matriz),
+                                         self.width, self.height)
+                    novo_estado.jogador = self.jogador
+                    # Coloca o jogador atual na celula i, j
+                    novo_estado.matriz[i][j] = self.jogador
+                    # Muda o jogador
+                    if (novo_estado.jogador == 1):
+                        novo_estado.jogador = 2
+                    else:
+                        novo_estado.jogador = 1
+                    # Adiciona o estado na lista de estados
+                    estados.append(novo_estado)
+        return estados
+
+    # string
+    def __str__(self):
+        return "Estado: \n" + str(self.matriz) + "\nJogador: " + str(self.jogador) + "\nVencedor: " + str(self.vencedor) + "\nEmpate: " + str(self.empate)
--- Prova 2/Game.py
+++ Prova 2/Game.py
@@ -10,7 +10,7 @@ width = 600
 height = 600
 
 # Cria um estado inicial com matriz zerada
-tamanho = 6
+tamanho = 3
 estado = Estado(tamanho, np.zeros((tamanho, tamanho)), width, height)
 
 
@@ -25,11 +25,14 @@ def setup():
     textAlign(CENTER, CENTER)  # Centraliza o texto horizontal e verticalmente
 
 
+coutner = 0
+
 # Função de desenho do p5
+
+
 def draw():
     # Limpa a tela
     background(204)
-
     global estado
     # Desenha o estado inicial
     estado.desenha()
@@ -41,7 +44,7 @@ def mouse_pressed():
     # Pega a posição do mouse
     x = mouse_x
     y = mouse_y
-    if (estado.vencedor == 0):
+    if (estado.vencedor == 0 and not estado.empate):
         # Considerando a espessura, verifica se algum centro de celula foi clicado
         for i in range(0, len(estado.centros_celulas)):
             if (x >= estado.centros_celulas[i][0] - estado.espessura/2 and x <= estado.centros_celulas[i][0] + estado.espessura/2 and y >= estado.centros_celulas[i][1] - estado.espessura/2 and y <= estado.centros_celulas[i][1] + estado.espessura/2):
@@ -50,19 +53,85 @@ def mouse_pressed():
                     # Se estiver vazia, coloca o jogador atual
                     estado.matriz[i // estado.size][i %
                                                     estado.size] = estado.jogador
-                    # Ve for o jogador 1, muda para o jogador 2
-                    if (estado.jogador == 1):
-                        if (estado.fim()):
-                            estado.vencedor = 1
-                            return
-                        estado.jogador = 2
-                    # Se for o jogador 2, muda para o jogador 1
-                    else:
-                        if (estado.fim()):
-                            estado.vencedor = 2
-                            return
-                        estado.jogador = 1
+
+                    if (estado.fim()):
+                        return
+                    estado.jogador = 2
+
+                    melhor_jogada()
+
+                    if (estado.fim()):
+                        return
+
+                    estado.jogador = 1
+
                 break
+
+# Função de tecla do p5
+
+
+def key_pressed():
+    print(key)
+    # se clicar em r, reseta o jogo
+    if (key == "r"):
+        global estado
+        estado = Estado(tamanho, np.zeros((tamanho, tamanho)), width, height)
+        estado.jogador = 1
+
+# Algoritmo melhor jogada
+
+
+def melhor_jogada():
+    global estado
+    melhor_pontuacao = math.inf
+    melhor_jogada_estado = None
+    for estado_novo in estado.estados_possiveis():
+        pontuacao = minimax(estado_novo, True, 0)
+
+        if (pontuacao < melhor_pontuacao):
+            melhor_pontuacao = pontuacao
+            melhor_jogada_estado = estado_novo
+
+    estado = melhor_jogada_estado
+
+
+def minimax(estadoMinMax, maximizando, profundidade=0):
+    if (estadoMinMax.fim()):
+        if (estadoMinMax.empate):
+            return 0
+        else:
+            multiplicador = 1 if estadoMinMax.vencedor == 1 else -1
+            if profundidade == 0:
+                return multiplicador * 10
+            elif profundidade == 1:
+                return multiplicador * 9
+            elif profundidade == 2:
+                return multiplicador * 8
+            elif profundidade == 3:
+                return multiplicador * 7
+            elif profundidade == 4:
+                return multiplicador * 6
+            elif profundidade == 5:
+                return multiplicador * 5
+            elif profundidade == 6:
+                return multiplicador * 4
+            elif profundidade == 7:
+                return multiplicador * 3
+            elif profundidade == 8:
+                return multiplicador * 2
+            else:
+                return multiplicador
+
+    pontucaoes = []
+
+    for estado_novo in estadoMinMax.estados_possiveis():
+        pontucaoes.append(
+            minimax(estado_novo, not maximizando, profundidade + 1))
+
+    if (maximizando):
+        return max(pontucaoes)
+    else:
+        return min(pontucaoes)
 
 
 # Roda o p5
