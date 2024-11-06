719ecc430641ed34538ccfa8750ce206cca10714
DeivisFelipe <guerreirodosul01@gmail.com>
2024-11-06 11:30:59 -0300

    fix: primeira versão do gera graficos com mongo pronta

--- AvaliadorFluxo/GeraGraficosMongo.py
+++ AvaliadorFluxo/GeraGraficosMongo.py
@@ -1,120 +1,47 @@
 import matplotlib.pyplot as plt
+import pymongo
+import time
 
-ARQUIVO_FLUXOS_ORDENADOS = "AvaliadorFluxo/Saida/FluxosOrdenados-Caida01.txt"
-PASTA_GRAFICOS = "AvaliadorFluxo/Saida/Graficos"
+PATH_GRAPHS = "AvaliadorFluxo/Saida/Graficos/AnaliseCaida"
 NAME = "Caida01"
-# PLOT
-QUANTIDADE_FLUXOS_HISTOGRAMA = 10
+NUMBER_BINS_HISTOGRAMA = 60
+DB_NAME = "fluxos_database"
+COLLECTION_NAME = "caida_collection"
 
 def main():
-    fluxos = []
-    maior_duracao = 0
-    menor_duracao = 9999999999999999
-    maior_bytes = 0
-    menor_bytes = 9999999999999999
-    # Lê o arquivo de fluxos
-    with open(ARQUIVO_FLUXOS_ORDENADOS, 'r') as f:
-        linhas = f.readlines()
-        # Pula as 5 primeiras linhas
-        for index, linha in enumerate(linhas[0:]):
-            # Separa a linha por espaços
-            partes = linha.split()
-            # Pega o src e a sport
-            src_port = partes[0].split(":")
-            if len(src_port) == 2:
-                src = src_port[0]
-                sport = src_port[1]
-            else:
-                # é ipv6
-                sport = src_port[-1]
-                # src é tudo menos a porta e os dois pontos
-                src = partes[0][:-len(sport) - 1]
+    # Conecta ao MongoDB
+    mongo_client = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = mongo_client[DB_NAME]
+    collection = db[COLLECTION_NAME]
 
-            # Pega o dst e a dport
-            dst_port = partes[2].split(":")
-            if len(dst_port) == 2:
-                dst = dst_port[0]
-                dport = dst_port[1]
-            else:
-                # é ipv6
-                dport = dst_port[-1]
-                # dst é tudo menos a porta e os dois pontos
-                dst = partes[2][:-len(dport) - 1]
-                
-            # Pega a quantidade de bytes enviados e recebidos, verifica se é em bytes, kB, MB ou GB
-            nspackges = int(partes[3])
-            sbytes = int(partes[4])
-            tsbytes = partes[5]
-            if tsbytes == "kB":
-                sbytes = sbytes * 1024
-            elif tsbytes == "MB":
-                sbytes = sbytes * 1024 * 1024
-            elif tsbytes == "GB":
-                sbytes = sbytes * 1024 * 1024 * 1024
+    # Pega o maior e o menor tempo de duração e a maior e a menor quantidade de bytes usando queries
+    bigger_duration = collection.find_one(sort=[("duration", pymongo.DESCENDING)])["duration"]
+    smaller_duration = collection.find_one(sort=[("duration", pymongo.ASCENDING)])["duration"]
+    bigger_bytes = collection.find_one(sort=[("nbytes_total", pymongo.DESCENDING)])["nbytes_total"]
+    smaller_bytes = collection.find_one(sort=[("nbytes_total", pymongo.ASCENDING)])["nbytes_total"]
 
-            nrpackges = int(partes[6])
-            rbytes = int(partes[7])
-            trbytes = partes[8]
-            if trbytes == "kB":
-                rbytes = rbytes * 1024
-            elif trbytes == "MB":
-                rbytes = rbytes * 1024 * 1024
-            elif trbytes == "GB":
-                rbytes = rbytes * 1024 * 1024 * 1024
+    print("Maior duração: ", bigger_duration)
+    print("Menor duração: ", smaller_duration)
+    print("Maior quantidade de bytes: ", bigger_bytes)
+    print("Menor quantidade de bytes: ", smaller_bytes)
 
-            ntpackges = int(partes[9])
-            tbytes = int(partes[10])
-            ttbytes = partes[11]
-            if ttbytes == "kB":
-                tbytes = tbytes * 1024
+    # Gera os intervalos de duração e bytes
+    duration_interval = (bigger_duration - smaller_duration) / NUMBER_BINS_HISTOGRAMA
+    duration_intervals = []
+    for i in range(NUMBER_BINS_HISTOGRAMA):
+        duration_intervals.append(round(smaller_duration + i * duration_interval))
 
-            rtime = float(partes[12].replace(",", "."))
-            duration = float(partes[13].replace(",", "."))
-
-            # Cria o fluxo
-            fluxos.append((src, sport, dst, dport, nspackges, sbytes, nrpackges, rbytes, ntpackges, tbytes, rtime, duration))
-
-            # Atualiza a maior e menor duração
-            if duration > maior_duracao:
-                maior_duracao = duration
-            if duration < menor_duracao:
-                menor_duracao = duration
-
-            # Atualiza a maior e menor quantidade de bytes
-            if tbytes > maior_bytes:
-                maior_bytes = tbytes
-            if tbytes < menor_bytes:
-                menor_bytes = tbytes
-    print("Total de fluxos: ", len(fluxos))
-    print("Maior duração: ", maior_duracao)
-    print("Menor duração: ", menor_duracao)
-    print("Maior quantidade de bytes: ", maior_bytes)
-    print("Menor quantidade de bytes: ", menor_bytes)
-
-    NUMERO_INTERVALOS_DURACAO = 60
-    intervalo_duracao = (maior_duracao - menor_duracao) / NUMERO_INTERVALOS_DURACAO
-    intervalos_duracao = []
-    for i in range(NUMERO_INTERVALOS_DURACAO):
-        # duas casas decimais
-        intervalos_duracao.append(round(menor_duracao + i * intervalo_duracao, 2))
-
-    # print("Duracao intervalos: ", intervalosDuracao)
-
-    NUMERO_INTERVALOS_BYTES = 60
-    intervalo_bytes = (maior_bytes - menor_bytes) / NUMERO_INTERVALOS_BYTES
-    intervalos_bytes = []
-    for i in range(NUMERO_INTERVALOS_BYTES):
-        # duas casas decimais
-        intervalos_bytes.append(round(menor_bytes + i * intervalo_bytes, 2))
-
-    # print("Bytes intervalos: ", intervalosBytes)
+    bytes_interval = (bigger_bytes - smaller_bytes) / NUMBER_BINS_HISTOGRAMA
+    bytes_intervals = []
+    for i in range(NUMBER_BINS_HISTOGRAMA):
+        bytes_intervals.append(round(smaller_bytes + i * bytes_interval))
 
     # Contador
-    contadorDuracao = [0 for i in range(NUMERO_INTERVALOS_DURACAO)]
-    contadorBytes = [0 for i in range(NUMERO_INTERVALOS_BYTES)]
-    contadorPacotes = [0 for i in range(NUMERO_INTERVALOS_DURACAO)]
+    contadorDuracao = [0 for i in range(NUMBER_BINS_HISTOGRAMA)]
+    contadorBytes = [0 for i in range(NUMBER_BINS_HISTOGRAMA)]
+    contadorPacotes = [0 for i in range(NUMBER_BINS_HISTOGRAMA)]
 
-    bytesSoma = [0 for i in range(NUMERO_INTERVALOS_DURACAO)]
+    bytesSoma = [0 for i in range(NUMBER_BINS_HISTOGRAMA)]
 
     # Conta quantos fluxos estão em cada intervalo
     for fluxo in fluxos:
@@ -122,8 +49,8 @@ def main():
         duracao = fluxo[11]
         bytes = fluxo[9]
         pacotes = fluxo[8]
-        for i in range(NUMERO_INTERVALOS_DURACAO):
-            if duracao >= intervalos_duracao[i]:
+        for i in range(NUMBER_BINS_HISTOGRAMA):
+            if duracao >= duration_intervals[i]:
                 index = i
             else:
                 break
@@ -135,7 +62,7 @@ def main():
         index = 0
         
         # descobre em qual intervalo está
-        for i in range(NUMERO_INTERVALOS_BYTES):
+        for i in range(NUMBER_BINS_HISTOGRAMA):
             if bytes >= intervalos_bytes[i]:
                 index = i
             else:
@@ -149,7 +76,7 @@ def main():
     plt.xlabel('Intervalos de duração')
     plt.ylabel('Quantidade de fluxos')
     plt.title('Histograma de duração dos fluxos - ' + NAME)
-    plt.savefig(PASTA_GRAFICOS + "/HistogramaDuracao.png")
+    plt.savefig(PATH_GRAPHS + "/HistogramaDuracao.png")
 
     # Cria o gráfico de linha com y em escala logarítmica
     plt.clf()
@@ -158,7 +85,7 @@ def main():
     plt.xlabel('Intervalos de bytes')
     plt.ylabel('Quantidade de fluxos')
     plt.title('Histograma de bytes dos fluxos - ' + NAME)
-    plt.savefig(PASTA_GRAFICOS + "/HistogramaBytes.png")
+    plt.savefig(PATH_GRAPHS + "/HistogramaBytes.png")
 
     # Tamanho médio dos pacotes
     # X igual a duracao 
@@ -173,7 +100,7 @@ def main():
     plt.xlabel('Intervalos de duração')
     plt.ylabel('Tamanho médio dos pacotes')
     plt.title('Tamanho médio dos pacotes em relação a duração - ' + NAME)
-    plt.savefig(PASTA_GRAFICOS + "/TamanhoMedioPacotes.png")
+    plt.savefig(PATH_GRAPHS + "/TamanhoMedioPacotes.png")
 
     # Número total de pacotes
     total_pacotes = sum(contadorPacotes)
@@ -188,4 +115,12 @@ def main():
     
 
 if __name__ == '__main__':
+    start_time = time.time()
+    print("Iniciando a geração dos gráficos...")
+    print("Horário:", time.strftime("%H:%M:%S", time.localtime(start_time)))
+    print("=" * 50)
     main()
+    print("=" * 50)
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print("Tempo de execução: ", execution_time, " segundos")
