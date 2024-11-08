e5b651cf4ae853926b675ba170fb6a755018efed
guerreirodosul01@gmail.com <guerreirodosul01@gmail.com>
2024-11-08 17:34:51 -0300

    feat: começando a convolução em CPP

---
+++ Cadeiras/Programacao Paralela/convolucao.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+#define PATH_IMAGE "imagens/imagem.jpg"
+#define SIZE_MASK 5
+#define BLUR_DIVISION 100
+
+
+using namespace std;
+
+
+class ImagemMatriz {
+    private:
+        int width;
+        int height;
+        int *red;
+        int *green;
+        int *blue;
+};
+
+int main() {
+
+    return 0;
+}
